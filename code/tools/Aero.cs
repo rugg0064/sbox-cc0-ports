30ded59111f839e7e6f31a28170b799f0fd99fa3
rugg0064 <35048262+rugg0064@users.noreply.github.com>
2021-08-19 19:02:40 -0500

    Initial

---
+++ code/tools/Aero.cs
@@ -0,0 +1,47 @@
+﻿namespace Sandbox.Tools
+{
+	[Library( "tool_areo", Title = "Aero", Description = "Creates aerodynamic surfaces", Group = "construction" )]
+	public partial class Aero : BaseTool
+	{
+
+		public override void Activate()
+		{
+			base.Activate();
+
+		}
+
+		protected override bool IsPreviewTraceValid( TraceResult tr )
+		{
+			if ( !base.IsPreviewTraceValid( tr ) )
+				return false;
+
+			return true;
+		}
+
+		public override void CreatePreviews()
+		{
+
+		}
+
+		public override void Simulate()
+		{
+			if ( !Host.IsServer )
+				return;
+
+			using ( Prediction.Off() )
+			{
+				if(Input.Pressed(InputButton.Attack1))
+				{
+					Vector3 startPos = Owner.EyePos;
+					Vector3 dir = Owner.EyeRot.Forward;
+					TraceResult tr = Trace.Ray( startPos, startPos + dir * MaxTraceDistance )
+					.Ignore( Owner )
+					.Run();
+					DebugOverlay.Line( tr.StartPos, tr.EndPos, 10.0f, false );
+					DebugOverlay.Line( tr.EndPos, tr.EndPos + tr.Normal * 64, 10.0f, false );
+
+				}
+			}
+		}
+	}
+}
